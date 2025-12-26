da7bd236252b7ed6ba839611088a2ac6a0d2e240
imam-hossain-wd <imamhossain6t9@gmail.com>
2025-12-26 20:56:50 +0600

    donate Hero Donation categories donation methods transparency section added in donate page

--- pages/donatepage/DonatePage.tsx
+++ pages/donatepage/DonatePage.tsx
@@ -1,9 +1,18 @@
+import DonateHero from "@/components/view/Donate/DonateHero";
+import DonationCategories from "@/components/view/Donate/DonationCategories";
+import DonationMethods from "@/components/view/Donate/DonationMethods";
+import TransparencySection from "@/components/view/Donate/TransparencySection";
 
 
-import React from 'react'
+
 
 export default function DonatePage() {
   return (
-    <div>DonatePage</div>
+    <div>
+      <DonateHero />
+      <DonationCategories />
+      <DonationMethods />
+      <TransparencySection />
+    </div>
   )
 }
